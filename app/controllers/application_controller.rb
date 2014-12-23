3fc59d158e51027f2f4d9fabfee1ad72b72a86da
Tejas Manohar <me@tejasmanohar.com>
2014-12-22 21:39:29 -0600

    fa-lg is too big

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,12 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
+  def index
+    if current_user
+      render 'index'
+    end
+  end
+
   private
     def current_user
       @current_user ||= User.find(session[:user_id]) if session[:user_id]
