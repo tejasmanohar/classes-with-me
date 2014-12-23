df81afceb54f9c193e53fac9cad70a84a560c22b
Tejas Manohar <me@tejasmanohar.com>
2014-12-22 21:25:24 -0600

    hardcode fb provider

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   root 'application#index'
-  get '/auth/:provider/callback' => 'sessions#create'
+  get '/auth/facebook/callback' => 'sessions#create'
   get '/signout' => 'sessions#destroy', :as => :signout
   get '/auth/failure' => 'sessions#failure'
 end
