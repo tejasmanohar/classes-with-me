0a0410a5688e39c30ee16e257bfac2384f9f9f4d
Tejas Manohar <me@tejasmanohar.com>
2014-12-22 20:18:32 -0600

    omniauth initializer

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :users
   root 'application#index'
   get '/auth/:provider/callback' => 'sessions#create'
-  get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
   get '/auth/failure' => 'sessions#failure'
 end
