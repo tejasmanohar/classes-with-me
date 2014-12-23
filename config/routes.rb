6647a4e147c193b241cdb0a57f495c619f6a907a
Tejas Manohar <me@tejasmanohar.com>
2014-12-22 22:59:12 -0600

    remove resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  resources :users
   root 'application#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signout' => 'sessions#destroy', :as => :signout
